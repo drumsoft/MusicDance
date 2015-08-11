4ea2dd3f09a37739d9e7e499720bb74b78304e9b
Haruka Kataoka <hrk8@drumsoft.com>
2015-08-11 16:40:46 +0900

    Net::OpenSoundControl custom version: bug fixed and some feature added.

--- osctests/oscdump.pl
+++ osctests/oscdump.pl
@@ -2,9 +2,12 @@
 
 use strict;
 use warnings;
-use Net::OpenSoundControl::Server;
+
 use Data::Dumper qw(Dumper);
 use Time::HiRes qw(time);
+
+use lib './lib';
+use Net::OpenSoundControl::Server;
 
 my $port = $ARGV[0] || 7771;
 
@@ -15,11 +18,15 @@ my $start = time();
 
 my $server =
   Net::OpenSoundControl::Server->new(
-    Port => $port, 
+    Port => $port, MaxLength => 1472,
     Handler => sub {
       print int((time() - $start) * 1000), "\t", Dumper($_[1]), "\n";
     })
   or die "Could not start oscdump: $@\n";
+
+$SIG{'INT'} = sub {
+  $server->stoploop();
+};
 
 print STDERR "[oscdump] Receiving messages on port $port\n";
 
