464ae9f5dbded10d6f36620cd674c8daf738df24
Haruka Kataoka <hrk8@drumsoft.com>
2015-08-27 04:15:30 +0900

    ループオプション -l と、引数にファイルパスを指定する機能を追加

--- osctests/oscsend.pl
+++ osctests/oscsend.pl
@@ -11,16 +11,24 @@ use Net::OpenSoundControl::Client;
 my $verbose = 0;
 my $host = "127.0.0.1";
 my $port = 7772;
+my $file;
+my $loop = 0;
 
-my $addr = $ARGV[0];
-if ($addr) {
-  if ($addr =~ /^(\d+\.\d+\.\d+\.\d+)/) {
-    $host = $1
-  }
-  if ($addr =~ /(?:^|\:)(\d+)$/) {
-    $port = $1;
+foreach (@ARGV) {
+  if ($_ eq '-l' || $_ eq '--loop') {
+    $loop = 1;
+  } elsif (/^[\d\.\:]+$/) {
+    if (/^(\d+\.\d+\.\d+\.\d+)/) {
+      $host = $1
+    }
+    if (/(?:^|\:)(\d+)$/) {
+      $port = $1;
+    }
+  } elsif (-e $_) {
+    $file = $_;
   }
 }
+
 
 my $start = time();
 
@@ -30,23 +38,41 @@ my $client =
 
 print STDERR "[oscsend] Sending out test messages to $host:$port\n";
 
+if ($file) {
+  open(STDIN, $file);
+}
+
 my $VAR1;
-while (<STDIN>) {
-  chomp;
-  my ($at, $dump) = split /\t/, $_, 2;
-  if ($at =~ /^\d+$/ && $dump) {
-    eval($dump);
-    if ($@) {
-      print STDERR "[oscsend] eval error at $at: $dump\n";
-      next;
+
+while (1) {
+  while (<STDIN>) {
+    chomp;
+    my ($at, $dump) = split /\t/, $_, 2;
+    if ($at =~ /^\d+$/ && $dump) {
+      eval($dump);
+      if ($@) {
+        print STDERR "[oscsend] eval error at $at: $dump\n";
+        next;
+      }
+      my $interval = ($at / 1000) + $start - time();
+      print STDERR "[oscsend] $interval: $dump\n" if $verbose;
+      if ($interval > 0) {
+        sleep($interval);
+      }
+      $client->send($VAR1);
     }
-    my $interval = ($at / 1000) + $start - time();
-    print STDERR "[oscsend] $interval: $dump\n" if $verbose;
-    if ($interval > 0) {
-      sleep($interval);
-    }
-    $client->send($VAR1);
   }
+  if ($loop) {
+    seek(STDIN, 0, 0);
+    $start = time();
+    print STDERR "[oscsend] looping.\n";
+  } else {
+    last;
+  }
+}
+
+if ($file) {
+  close(STDIN);
 }
 
 print STDERR "[oscsend] Send finished.\n";
